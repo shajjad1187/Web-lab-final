2c3f418119ebbc72e95e41598c953c35e8fdd113
shajjad1187 <73533696+shajjad1187@users.noreply.github.com>
2020-12-19 22:58:00 +0600

    Add files via upload

---
+++ Solve-1-index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <body>
+	<form method="post" action="process.php">
+		name:<br>
+		<input type="text" name="name">
+		<br>
+		id:<br>
+		<input type="id" name="id">
+		<br>
+		age:<br>
+		<input type="text" name="age">
+		<br>
+		user_name:<br>
+		<input type="text" name="user_name">
+		<br>
+		password:<br>
+		<input type="text" name="password">
+		<br>
+		<br>
+		<input type="submit" name="save" value="submit">
+	</form>
+
+	</table>
+  </body>
+</html>
---
+++ Solve-3-index.php
@@ -0,0 +1,227 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>ABC COLLEGE</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <style>
+  body {
+    font: 400 15px Lato, sans-serif;
+    line-height: 1.8;
+    color: #818181;
+  }
+  h2 {
+    font-size: 24px;
+    text-transform: uppercase;
+    color: #303030;
+    font-weight: 600;
+    margin-bottom: 30px;
+  }
+  h4 {
+    font-size: 19px;
+    line-height: 1.375em;
+    color: #303030;
+    font-weight: 400;
+    margin-bottom: 30px;
+  }  
+  .jumbotron {
+    background-color: #61f26e;
+    color: #fff;
+    padding: 100px 25px;
+    font-family: Montserrat, sans-serif;
+  }
+  .container-fluid {
+    padding: 60px 50px;
+  }
+  .bg-grey {
+    background-color: #f6f6f6;
+  }
+  .logo-small {
+    color: #f4511e;
+    font-size: 50px;
+  }
+  .logo {
+    color: #f4511e;
+    font-size: 200px;
+  }
+  .thumbnail {
+    padding: 0 0 15px 0;
+    border: none;
+    border-radius: 0;
+  }
+  .thumbnail img {
+    width: 100%;
+    height: 100%;
+    margin-bottom: 10px;
+  }
+  .carousel-control.right, .carousel-control.left {
+    background-image: none;
+    color: #f4511e;
+  }
+  .carousel-indicators li {
+    border-color: #f4511e;
+  }
+  .carousel-indicators li.active {
+    background-color: #f4511e;
+  }
+  .item h4 {
+    font-size: 19px;
+    line-height: 1.375em;
+    font-weight: 400;
+    font-style: italic;
+    margin: 70px 0;
+  }
+  .item span {
+    font-style: normal;
+  }
+  .panel {
+    border: 1px solid #f4511e; 
+    border-radius:0 !important;
+    transition: box-shadow 0.5s;
+  }
+  .panel:hover {
+    box-shadow: 5px 0px 40px rgba(0,0,0, .2);
+  }
+  .panel-footer .btn:hover {
+    border: 1px solid #f4511e;
+    background-color: #fff !important;
+    color: #f4511e;
+  }
+  .panel-heading {
+    color: #fff !important;
+    background-color: #f4511e !important;
+    padding: 25px;
+    border-bottom: 1px solid transparent;
+    border-top-left-radius: 0px;
+    border-top-right-radius: 0px;
+    border-bottom-left-radius: 0px;
+    border-bottom-right-radius: 0px;
+  }
+  .panel-footer {
+    background-color: white !important;
+  }
+  .panel-footer h3 {
+    font-size: 32px;
+  }
+  .panel-footer h4 {
+    color: #aaa;
+    font-size: 14px;
+  }
+  .panel-footer .btn {
+    margin: 15px 0;
+    background-color: #f4511e;
+    color: #fff;
+  }
+  .navbar {
+    margin-bottom: 0;
+    background-color: #f4511e;
+    z-index: 9999;
+    border: 0;
+    font-size: 12px !important;
+    line-height: 1.42857143 !important;
+    letter-spacing: 4px;
+    border-radius: 0;
+    font-family: Montserrat, sans-serif;
+  }
+  .navbar li a, .navbar .navbar-brand {
+    color: #fff !important;
+  }
+  .navbar-nav li a:hover, .navbar-nav li.active a {
+    color: #f4511e !important;
+    background-color: #fff !important;
+  }
+  .navbar-default .navbar-toggle {
+    border-color: transparent;
+    color: #fff !important;
+  }
+  footer .glyphicon {
+    font-size: 20px;
+    margin-bottom: 20px;
+    color: #f4511e;
+  }
+  
+  
+  </style>
+</head>
+<body id="home" data-spy="scroll" data-target=".navbar" data-offset="60">
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#home">Home</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#about">About</a></li>
+        <li><a href="#teacher_info">Teacher Info</a></li>
+        <li><a href="#notice_board">Notice Board</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="jumbotron text-center">
+  <h1>ABC COLLEGE</h1>
+</div>
+
+<div id="about" class="container-fluid">
+  <div class="row">
+    <div class="col-sm-8">
+      <h2>About</h2><br>
+      <h3>ABC College is an educational institution in Bangladesh established in 1965. Although the school was set up to cater for the children of surrounding areas, students come from all parts of Dhaka. The institution has three campuses inside Dhaka in Motijheel,Banasree and Mugda.</p>
+     
+    </div>
+  </div>
+</div>
+
+<div id="teacher_info" class="container-fluid">
+  <div class="row">
+    <div class="col-sm-8">
+      <h2>Teacher info</h2><br>
+      <h1>1</h1>
+      <h3>Name:Mithun Ahmed Pranta</h3>
+      <p>Phone:<span class="glyphicon glyphicon-phone"></span> 01735505030</p>
+      <p>Email:<span class="glyphicon glyphicon-envelope"></span> mithun15-1117@diu.edu.bd</p>
+      <p>Address:<span class="glyphicon glyphicon-map-marker"></span> Dhaka,BD</p>
+      <h1>2</h1>
+     <h3>Name:Abdullah Al Mithun</h3>
+     <p>Phone:<span class="glyphicon glyphicon-phone"></span> 01700000000</p>
+      <p>Email:<span class="glyphicon glyphicon-envelope"></span> abdullah15-1120@diu.edu.bd</p>
+      <p>Address:<span class="glyphicon glyphicon-map-marker"></span> Dhaka,BD</p>
+    </div>
+  </div>
+</div>
+
+<div id="notice_board" class="container-fluid">
+  <div class="row">
+    <div class="col-sm-8">
+      <h2>Notice Board</h2><br>
+      <h1>1.The college is remain closed in 16th dec 2020.</p>
+     
+    </div>
+  </div>
+</div>
+
+<div id="contact" class="container-fluid bg-grey">
+  <h2 class="text-center">CONTACT</h2>
+  <div class="row">
+    <div class="col-sm-5">
+      <p>Contact us and we'll get back to you within 24 hours.</p>
+      <p><span class="glyphicon glyphicon-map-marker"></span> Dhaka,BD</p>
+      <p><span class="glyphicon glyphicon-phone"></span> 01735505030</p>
+      <p><span class="glyphicon glyphicon-envelope"></span> mithun15-1117@diu.edu.bd</p>
+  </div>
+</div>
+
+</body>
+</html>
